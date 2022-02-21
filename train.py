c94c8d650443950dc72638bcc088080b101722cb
cristiandatum <cristian.alberch@outlook.com>
2022-02-21 10:26:18 +0100

    train.py saving model

--- train.py
+++ train.py
@@ -63,13 +63,15 @@ def main():
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
